d183f0b6998c7fbf376b6ff35dcdd559217ebace
hicka04 <hicka04@gmail.com>
2019-12-12 16:03:40 +0900

    依存ライブラリ追加

--- Package.swift
+++ Package.swift
@@ -13,16 +13,24 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/ishkawa/APIKit.git", .upToNextMinor(from: "5.0.0")),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMinor(from: "2.2.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMinor(from: "8.0.4")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "QiitaAPI",
-            dependencies: []),
+            dependencies: [
+                "APIKit"
+            ]),
         .testTarget(
             name: "QiitaAPITests",
-            dependencies: ["QiitaAPI"]),
+            dependencies: [
+                "QiitaAPI",
+                "Quick",
+                "Nimble"
+            ]),
     ]
 )
