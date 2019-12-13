e23be1eb5a069f51b9ba2c0b08b9870d195596bf
hicka04 <hicka04@gmail.com>
2019-12-13 09:38:51 +0900

    APIクライアント追加

--- Sources/QiitaAPI/QiitaAPI.swift
+++ Sources/QiitaAPI/QiitaAPI.swift
@@ -1,3 +1,21 @@
-struct QiitaAPI {
-    var text = "Hello, World!"
+//
+//  QiitaAPI.swift
+//  
+//
+//  Created by hicka04 on 2019/12/12.
+//
+
+import Foundation
+import APIKit
+
+public final class QiitaAPI {
+    
+    public init () {}
+    
+    public func send<Request: QiitaRequest>(_ request: Request,
+                                            completion: @escaping (Result<Request.Response, SessionTaskError>) -> Void) {
+        Session.send(request) { result in
+            completion(result)
+        }
+    }
 }
--- Tests/QiitaAPITests/QiitaAPITests.swift
+++ Tests/QiitaAPITests/QiitaAPITests.swift
@@ -1,12 +1,22 @@
 import XCTest
-@testable import QiitaAPI
+import QiitaAPI
 
 final class QiitaAPITests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(QiitaAPI().text, "Hello, World!")
+        let apiExpectation = expectation(description: "api request")
+        QiitaAPI().send(SearchItems()) { result in
+            switch result {
+            case .success(let response):
+                print(response)
+            case .failure(let error):
+                print(error)
+            }
+            apiExpectation.fulfill()
+        }
+        wait(for: [apiExpectation], timeout: 2)
     }
 
     static var allTests = [
