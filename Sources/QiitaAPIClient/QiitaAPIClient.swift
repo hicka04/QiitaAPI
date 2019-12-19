337c63d2fbf19828d670c577b1355c812e1da96b
hicka04 <hicka04@gmail.com>
2019-12-19 16:18:09 +0900

    クロージャのメソッド削除

--- Sources/QiitaAPIClient/QiitaAPIClient.swift
+++ Sources/QiitaAPIClient/QiitaAPIClient.swift
@@ -16,34 +16,6 @@ public final class QiitaAPIClient {
         self.session = session
     }
     
-    public func send<Request: QiitaRequest>(_ request: Request,
-                                            completion: @escaping (Result<Request.Response, QiitaClientError>) -> Void) {
-        let urlRequest = request.buildURLRequest()
-        print(urlRequest.url ?? "")
-        
-        let task = session.dataTask(with: urlRequest) { data, response, error in
-            switch (data, response, error) {
-            case (_, _, let error?):
-                completion(.failure(.connectionError(error)))
-                
-            case (let data?, let response?, _):
-                do {
-                    let response = try request.response(from: data, urlResponse: response)
-                    completion(.success(response))
-                } catch let error as ErrorResponse {
-                    completion(.failure(.apiError(error)))
-                } catch {
-                    completion(.failure(.responseParseError(error)))
-                }
-                
-            default:
-                fatalError("Invalid response combination")
-            }
-        }
-        
-        task.resume()
-    }
-    
     public func send<Request: QiitaRequest>(_ request: Request) -> AnyPublisher<Request.Response, QiitaClientError> {
         let urlRequest = request.buildURLRequest()
         print(urlRequest.url ?? "")
