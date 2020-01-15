9597412838887a0615386ac5a613b7285060eeb3
hicka04 <hicka04@gmail.com>
2020-01-15 17:27:16 +0900

    ユーザーの情報を追加

--- Sources/QiitaAPIClient/Response/User.swift
+++ Sources/QiitaAPIClient/Response/User.swift
@@ -10,6 +10,10 @@ import Foundation
 public struct User: Decodable, Identifiable, Equatable {
     
     public let id: ID
+    public let name: String
+    public let description: String?
+    public let location: String?
+    public let organization: String?
     public let profileImageUrl: URL
 }
 
